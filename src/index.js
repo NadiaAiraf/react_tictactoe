3fbf663be950a6d6a0ea0e08547660337a42b96f
NadiaAiraf <aidanfaria@gmail.com>
2018-12-03 13:53:51 +0000

    wining/losing/drawing

--- src/index.js
+++ src/index.js
@@ -10,6 +10,36 @@ function Square(props) {
   );
 }
 
+function calculateWinner(squares) {
+  const lines = [
+   [0, 1, 2],
+   [3, 4, 5],
+   [6, 7, 8],
+   [0, 3, 6],
+   [1, 4, 7],
+   [2, 5, 8],
+   [0, 4, 8],
+   [2, 4, 6],
+ ];
+ for (let i = 0; i < lines.length; i++) {
+   const [a, b, c] = lines[i];
+   if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+     return squares[a];
+   }
+ }
+ return null;
+}
+
+function full(squares) {
+  let full = true;
+  for (var i = 0; i < squares.length; i++) {
+    if (squares[i] === null) {
+      full = false;
+    }
+  }
+  return full;
+}
+
 class Board extends React.Component {
   constructor(props) {
     super(props);
@@ -29,6 +59,9 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O'
     this.setState({
       squares: squares,
@@ -37,7 +70,15 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner
+    } else if (full(this.state.squares)) {
+      status = 'DRAW'
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
